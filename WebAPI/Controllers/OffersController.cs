c8daa93e6a9885217f56f936c747e291d3aeb9a7
Rozalia Solecka <rozaliasolecka@gmail.com>
2022-05-08 22:45:42 +0200

    connect to front 1

--- WebAPI/Controllers/OffersController.cs
+++ WebAPI/Controllers/OffersController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MassTransit;
 using Models.Offers;
+using Models.Offers.Dto;
 
 namespace WebAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class OffersController : ControllerBase
     {
         readonly IRequestClient<GetOffersEvent> _client;
@@ -14,10 +17,11 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<GetOffersReplyEvent>> Offers(/* parametry */)
+        [Route("GetOffers")]
+        public async Task<IEnumerable<TripDto>> GetOffers(/* parametry */)
         {
             var response = await _client.GetResponse<GetOffersReplyEvent>(new GetOffersEvent("Albania", DateTime.Now, DateTime.Now, 3));
-            return response.Message;
+            return response.Message.Trips;
         }
     }
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -27,7 +27,11 @@ builder.Services.AddMassTransit(cfg =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
