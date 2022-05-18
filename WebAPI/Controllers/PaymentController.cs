d15fbbc8a7cba4798a9d0f7eb4c04b8f6efc7ad9
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2022-05-18 21:47:09 +0200

    Added payment controller

---
+++ WebAPI/Controllers/PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using Models.Reservations;
+using Microsoft.AspNetCore.Mvc;
+using MassTransit;
+using Models.Payments;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        readonly IRequestClient<PaymentInformationForReservationEvent> _client;
+        public PaymentController(IRequestClient<PaymentInformationForReservationEvent> client)
+        {
+            _client = client;
+        }
+
+        [HttpPost]
+        [Route("SendInformation")]
+        public async Task<Guid> SendInformation([FromBody] PaymentInformation paymentInformation)
+        {
+            var card = new CardCredentials()
+            {
+                CVV = paymentInformation.CVV,
+                Number = paymentInformation.Number,
+                ExpDate = paymentInformation.ExpDate,
+                FullName = paymentInformation.FullName
+            };
+            var request = new PaymentInformationForReservationEvent(card, 0);
+            var response = await _client.GetResponse<PaymentInformationForReservationReplyEvent>(request);
+            return response.Message.CorrelationId;
+        }
+    }
+}
---
+++ WebAPI/PaymentInformation.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI
+{
+    public class PaymentInformation
+    {
+        public string Number { get; set; }
+        public int CVV { get; set; }
+        public string ExpDate { get; set; }
+        public string FullName { get; set; }
+        public Guid ReservationId { get; set; }
+    }
+}
